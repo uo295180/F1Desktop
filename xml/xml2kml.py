cc9e52e700de32f4a2cdbddbacc91eb014ea667a
orvizz <153991700+orvizz@users.noreply.github.com>
2024-10-24 20:48:51 +0200

    planimetría y conversor de python creados

--- xml/xml2kml.py
+++ xml/xml2kml.py
@@ -15,31 +15,32 @@ def convertToKml(archivo):
     kml = etree.Element('kml', xmlns="http://www.opengis.net/kml/2.2")
     document = etree.SubElement(kml, 'Document')
 
-    salida = raiz.find('.//ns:salida', NAMESPACE)
-    if salida is not None:
-        longitud_salida = salida.find('.//ns:longitud/ns:gradosLongitud', NAMESPACE).text
-        latitud_salida = salida.find('.//ns:latitud/ns:gradosLatitud', NAMESPACE).text
-        altitud_salida = salida.find('.//ns:altitud', NAMESPACE).text
+    etree.SubElement(document, 'name').text = "Planimetría"
+    placemark = etree.SubElement(document, 'Placemark')
+    etree.SubElement(placemark, 'name').text = 'Circuito'
 
-        placemark_salida = etree.SubElement(document, 'Placemark')
-        etree.SubElement(placemark_salida, 'name').text = "Salida"
+    lookAt = etree.SubElement(placemark, 'LookAt')
+    longitud = raiz.find('.//ns:salida/ns:coordenadas/ns:longitud/ns:gradosLongitud', NAMESPACE).text
+    latitud = raiz.find('.//ns:salida/ns:coordenadas/ns:latitud/ns:gradosLatitud', NAMESPACE).text
+    altitud = raiz.find('.//ns:salida/ns:coordenadas/ns:altitud', NAMESPACE).text
+    etree.SubElement(lookAt, 'longitude').text = longitud
+    etree.SubElement(lookAt, 'latitude').text = latitud
+    etree.SubElement(lookAt, 'altitude').text = altitud
+    etree.SubElement(lookAt, 'altitudeMode').text = 'absolute'
 
-        point_salida = etree.SubElement(placemark_salida, 'Point')
-        coordenadas_salida = f"{longitud_salida},{latitud_salida},{altitud_salida}"
-        etree.SubElement(point_salida, 'coordinates').text = coordenadas_salida
-
+    polygon = etree.SubElement(placemark, 'Polygon')
+    outerBoundaryIs = etree.SubElement(polygon, 'outerBoundaryIs')
+    linearRing = etree.SubElement(outerBoundaryIs, 'LinearRing')
+    
+    textCoord = ""
     for tramo in raiz.findall('.//ns:tramo', NAMESPACE):
         longitud = tramo.find('.//ns:longitud/ns:gradosLongitud', NAMESPACE).text
         latitud = tramo.find('.//ns:latitud/ns:gradosLatitud', NAMESPACE).text
         altitud = tramo.find('.//ns:altitud', NAMESPACE).text
 
-        placemark = etree.SubElement(document, 'Placemark')
-        distancia = tramo.find('.//ns:distancia', NAMESPACE).text
-        etree.SubElement(placemark, 'name').text = f"Tramo - {distancia} metros"
+        textCoord += f"{longitud},{latitud},{altitud} "
 
-        punto = etree.SubElement(placemark, 'Point')
-        coordenadas = f"{longitud},{latitud},{altitud}"
-        etree.SubElement(punto, 'coordinates').text = coordenadas
+    etree.SubElement(linearRing, 'coordinates').text = textCoord
 
     archivoSalida = os.path.join(absolute_path, 'circuito.kml')
     arbol = etree.ElementTree(kml)
@@ -51,10 +52,25 @@ def convertToKml(archivo):
 
 def main():    
 
-    miArchivoXML = input('Introduzca un archivo XML = ')
+    #miArchivoXML = input('Introduzca un archivo XML = ')
+    miArchivoXML = "circuitoEsquema.xml"
     miArchivoXML = os.path.join(absolute_path,miArchivoXML)
     
     convertToKml(miArchivoXML)
 
 if __name__ == "__main__":
     main()   
+
+
+#    for tramo in raiz.findall('.//ns:tramo', NAMESPACE):
+#        longitud = tramo.find('.//ns:longitud/ns:gradosLongitud', NAMESPACE).text
+#        latitud = tramo.find('.//ns:latitud/ns:gradosLatitud', NAMESPACE).text
+#        altitud = tramo.find('.//ns:altitud', NAMESPACE).text
+#
+#        placemark = etree.SubElement(document, 'Placemark')
+#        distancia = tramo.find('.//ns:distancia', NAMESPACE).text
+#        etree.SubElement(placemark, 'name').text = f"Tramo - {distancia} metros"
+#
+#        punto = etree.SubElement(placemark, 'Point')
+#        coordenadas = f"{longitud},{latitud},{altitud}"
+#        etree.SubElement(punto, 'coordinates').text = coordenadas
